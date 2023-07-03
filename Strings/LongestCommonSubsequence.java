f3f949a2a322a2b7e6bd3c9de7a544e97a080bbf
Anant-Singh1 <97743499+Anant-Singh1@users.noreply.github.com>
2023-07-03 10:06:16 +0530

    Leetcode Problems

--- Strings/LongestCommonSubsequence.java
+++ Strings/LongestCommonSubsequence.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 public class LongestCommonSubsequence {
-    
+    private static void storeSubsequence(String str, ArrayList<String> ss) {
+        for (int i = 0; i < str.length(); i++) {
+            char firstCharacter = str.charAt(i);
+            if (ss.size() == 0) {
+                ss.add(" ");
+                ss.add("" + firstCharacter);
+                continue;
+            }
+            int listsize = ss.size();
+            for (int j = 0; j < listsize; j++) {
+                String temp = ss.get(j) + firstCharacter;
+                ss.add(temp);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str1 = "abcdxy";
+        String str2 = "abc";
+        ArrayList<String> ss1 = new ArrayList<>();
+        ArrayList<String> ss2 = new ArrayList<>();
+        storeSubsequence(str1, ss1);
+        storeSubsequence(str2, ss2);
+        /*
+         * System.out.println(ss1);
+         * System.out.println(ss2);
+         */
+        String lcs = "";
+        for (int i = 0; i < ss1.size(); i++) {
+            if (ss2.contains(ss1.get(i)))
+                lcs = ss1.get(i);
+        }
+
+    }
 }
---
+++ Strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet();
+        int max = 0;
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (!set.add(s.charAt(right))) {
+                set.remove(s.charAt(left++));
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
